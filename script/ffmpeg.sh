73e13a8b1aa6916dc9ca0a628960f1e04e3049ca
姚彪 <1315508912@qq.com>
2023-12-16 01:08:32 +0800

    ci: 修复构建

--- script/ffmpeg.sh
+++ script/ffmpeg.sh
@@ -21,27 +21,34 @@ download_ffmpeg_macos() {
 }
 
 download_ffmpeg_linux() {
-  local realArch=""
-  case $arch in
-  x64)
-    realArch="amd64"
-    ;;
-  arm64)
-    realArch="arm64"
-    ;;
-  esac
-  output=$ffmpeg_save_path/linux-x64/ffmpeg
-  local url="https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.4.1-${realArch}-static.tar.xz"
-  if [ ! -f "ffmpeg.tar.xz" ]; then
-    curl -kL $url -o "${download_dir}/ffmpeg.tar.xz"
-  fi
-
-  if [ ! -d "$output" ]; then
-    mkdir -p "$output"
-  fi
-  tar -xf "${download_dir}/ffmpeg.tar.xz" --strip-components 1 -C "$output" '*/ffmpeg'
-  chmod +x "$output/ffmpeg"
+  local url="https://github.com/eugeneware/ffmpeg-static/releases/download/b4.4.1/linux-$arch"
+  create_dir "$ffmpeg_save_path/linux-$arch/ffmpeg"
+  curl -kL "$url" -o "$ffmpeg_save_path/linux-$arch/ffmpeg/ffmpeg"
+  chmod +x "$ffmpeg_save_path/linux-$arch/ffmpeg/ffmpeg"
 }
+
+#download_ffmpeg_linux() {
+#  local realArch=""
+#  case $arch in
+#  x64)
+#    realArch="amd64"
+#    ;;
+#  arm64)
+#    realArch="arm64"
+#    ;;
+#  esac
+#  output=$ffmpeg_save_path/linux-x64/ffmpeg
+#  local url="https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.4.1-${realArch}-static.tar.xz"
+#  if [ ! -f "ffmpeg.tar.xz" ]; then
+#    curl -kL $url -o "${download_dir}/ffmpeg.tar.xz"
+#  fi
+#
+#  if [ ! -d "$output" ]; then
+#    mkdir -p "$output"
+#  fi
+#  tar -xf "${download_dir}/ffmpeg.tar.xz" --wildcards --strip-components 1 -C "$output" '*/ffmpeg'
+#  chmod +x "$output/ffmpeg"
+#}
 
 if [ "$os" == "mac" ]; then
   download_ffmpeg_macos
