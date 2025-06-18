13d4f7bfeb493e0ba8790116dfe6c33553b85461
to_mo_to <70244515+NLick47@users.noreply.github.com>
2025-06-18 21:29:38 +0800

    refactor: 请求由WebRequest替换为HttpClient (#314)
    * refactor: 请求由WebRequest替换为HttpClient
    
    * 添加代理
    
    * 设置PooledConnectionLifetime
    
    * 多余方法移除

--- DownKyi.Core/BiliApi/WebClient.cs
+++ DownKyi.Core/BiliApi/WebClient.cs
@@ -11,6 +11,51 @@ namespace DownKyi.Core.BiliApi;
 
 internal static class WebClient
 {
+    private static readonly HttpClient _httpClient;
+    private static string? _bvuid3 = string.Empty;
+    private static string? _bvuid4 = string.Empty;
+
+    static WebClient()
+    {
+        var socketsHandler = new SocketsHttpHandler
+        {
+            PooledConnectionLifetime = TimeSpan.FromMinutes(10), 
+            PooledConnectionIdleTimeout = TimeSpan.FromMinutes(5),
+            AutomaticDecompression = DecompressionMethods.All,
+            ConnectTimeout = TimeSpan.FromSeconds(10) 
+        };
+        switch (SettingsManager.GetInstance().GetNetworkProxy())
+        {
+            case NetworkProxy.None:
+                socketsHandler.UseProxy = false;
+                socketsHandler.Proxy = null;
+                break;
+            case NetworkProxy.System:
+                socketsHandler.UseProxy = true;
+                socketsHandler.Proxy = WebRequest.GetSystemWebProxy();
+                break;
+            case NetworkProxy.Custom:
+            {
+                try
+                {
+                    socketsHandler.UseProxy = true;
+                    socketsHandler.Proxy = new WebProxy(SettingsManager.GetInstance().GetCustomProxy());
+                }
+                catch (Exception e)
+                {
+                    socketsHandler.UseProxy = true;
+                    Console.WriteLine(e);
+                }
+            }
+                break;
+        }
+        
+        _httpClient = new HttpClient(socketsHandler);
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", SettingsManager.GetInstance().GetUserAgent());
+        _httpClient.DefaultRequestHeaders.Add("accept-language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7");
+        
+    }
+
     internal class SpiOrigin
     {
         [JsonPropertyName("data")] public Spi? Data { get; init; }
@@ -24,23 +69,6 @@ internal static class WebClient
         [JsonPropertyName("b_4")] public string? Bvuid4 { get; set; }
     }
 
-    private static string? _bvuid3 = string.Empty;
-    private static string? _bvuid4 = string.Empty;
-
-    // private static string GetRandomBuvid3()
-    // {
-    //     // 随机生成10位字符串
-    //     const string str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    //     var random = new Random();
-    //     var result = new StringBuilder();
-    //     for (var i = 0; i < 10; i++)
-    //     {
-    //         result.Append(str[random.Next(str.Length)]);
-    //     }
-    //
-    //     return result.ToString();
-    // }
-
     private static void GetBuvid()
     {
         const string url = "https://api.bilibili.com/x/frontend/finger/spi";
@@ -50,41 +78,12 @@ internal static class WebClient
         _bvuid4 = spi?.Data?.Bvuid4;
     }
 
-    /// <summary>
-    /// 发送get或post请求
-    /// </summary>
-    /// <param name="url"></param>
-    /// <param name="referer"></param>
-    /// <param name="method"></param>
-    /// <param name="parameters"></param>
-    /// <param name="retry"></param>
-    /// <returns></returns>
     public static string RequestWeb(string url, string? referer = null, string method = "GET",
         Dictionary<string, string>? parameters = null, int retry = 3)
     {
-        // 重试次数
         if (retry <= 0)
         {
             return "";
-        }
-
-        // post请求，发送参数
-        if (method == "POST" && parameters != null)
-        {
-            var builder = new StringBuilder();
-            var i = 0;
-            foreach (var item in parameters)
-            {
-                if (i > 0)
-                {
-                    builder.Append('&');
-                }
-
-                builder.Append($"{item.Key}={item.Value}");
-                i++;
-            }
-
-            url += "?" + builder;
         }
 
         try
@@ -94,47 +93,16 @@ internal static class WebClient
                 GetBuvid();
             }
 
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = method;
-            request.Timeout = 30 * 1000;
+            var request = new HttpRequestMessage(new HttpMethod(method), url);
 
-            request.UserAgent = SettingsManager.GetInstance().GetUserAgent();
-
-            //request.ContentType = "application/json,text/html,application/xhtml+xml,application/xml;charset=UTF-8";
-            request.Headers["accept-language"] = "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7";
-            request.Headers["accept-encoding"] = "gzip, deflate, br";
-            switch (SettingsManager.GetInstance().GetNetworkProxy())
-            {
-                case NetworkProxy.None:
-                    request.Proxy = null;
-                    break;
-                case NetworkProxy.System:
-                    request.Proxy = WebRequest.GetSystemWebProxy();
-                    break;
-                case NetworkProxy.Custom:
-                {
-                    try
-                    {
-                        request.Proxy = new WebProxy(SettingsManager.GetInstance().GetCustomProxy());
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                    }
-                }
-                    break;
-            }
-
-            // referer
             if (referer != null)
             {
-                request.Referer = referer;
+                request.Headers.Referrer = new Uri(referer);
             }
 
-            // 构造cookie
             if (!url.Contains("getLogin"))
             {
-                request.Headers["origin"] = "https://www.bilibili.com";
+                request.Headers.Add("origin", "https://www.bilibili.com");
 
                 var cookies = LoginHelper.GetLoginInfoCookiesString();
 
@@ -147,50 +115,33 @@ internal static class WebClient
                 {
                     cookies += $"; buvid4={_bvuid4}";
                 }
+
                 if (cookies is not "")
                 {
                     request.Headers.Add("cookie", cookies);
                 }
             }
 
-            var html = string.Empty;
-            using var response = (HttpWebResponse)request.GetResponse();
-            if (response.ContentEncoding.ToLower().Contains("gzip"))
+            if (method == "POST" && parameters != null)
             {
-                using var stream = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress);
-                using var reader = new StreamReader(stream, Encoding.UTF8);
-                html = reader.ReadToEnd();
+                request.Content = new FormUrlEncodedContent(parameters);
             }
-            else if (response.ContentEncoding.ToLower().Contains("deflate"))
+            else if (parameters != null)
             {
-                using var stream = new DeflateStream(response.GetResponseStream(), CompressionMode.Decompress);
-                using var reader = new StreamReader(stream, Encoding.UTF8);
-                html = reader.ReadToEnd();
-            }
-            else if (response.ContentEncoding.ToLower().Contains("br"))
-            {
-                using var stream = new BrotliStream(response.GetResponseStream(), CompressionMode.Decompress);
-                using var reader = new StreamReader(stream, Encoding.UTF8);
-                html = reader.ReadToEnd();
-            }
-            else
-            {
-                using var stream = response.GetResponseStream();
-                using var reader = new StreamReader(stream, Encoding.UTF8);
-                html = reader.ReadToEnd();
+                var query = string.Join("&", parameters.Select(kvp => $"{kvp.Key}={kvp.Value}"));
+                url = $"{url}?{query}";
+                request.RequestUri = new Uri(url);
             }
 
-            return html;
+            var response = _httpClient.Send(request);
+            response.EnsureSuccessStatusCode();
+            
+            using var reader = new StreamReader(response.Content.ReadAsStream());
+            return reader.ReadToEnd();
         }
-        catch (WebException e)
+        catch (HttpRequestException e)
         {
-            Console.WriteLine("RequestWeb()发生Web异常: {0}", e);
-            LogManager.Error(e);
-            return RequestWeb(url, referer, method, parameters, retry - 1);
-        }
-        catch (IOException e)
-        {
-            Console.WriteLine("RequestWeb()发生IO异常: {0}", e);
+            Console.WriteLine("RequestWeb()发生HTTP请求异常: {0}", e);
             LogManager.Error(e);
             return RequestWeb(url, referer, method, parameters, retry - 1);
         }
@@ -204,30 +155,36 @@ internal static class WebClient
 
     public static void DownloadFile(string url, string destFile, string? referer = null)
     {
-        var handler = new HttpClientHandler();
-
-        var client = new HttpClient(handler);
-        client.Timeout = TimeSpan.FromSeconds(30);
-        client.DefaultRequestHeaders.Add("User-Agent", SettingsManager.GetInstance().GetUserAgent());
-
-        if (referer != null)
+        try
         {
-            client.DefaultRequestHeaders.Add("Referer", referer);
-        }
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-        if (!url.Contains("getLogin"))
-        {
-            client.DefaultRequestHeaders.Add("origin", "https://m.bilibili.com");
-            var cookies = LoginHelper.GetLoginInfoCookiesString();
-            if (cookies is not "")
+            if (referer != null)
             {
-                client.DefaultRequestHeaders.Add("cookie", cookies);
+                request.Headers.Referrer = new Uri(referer);
             }
-        }
 
-        var responseMessage = client.GetAsync(url).Result;
-        if (!responseMessage.IsSuccessStatusCode) return;
-        using var fs = File.Create(destFile);
-        responseMessage.Content.ReadAsStream().CopyTo(fs);
+            if (!url.Contains("getLogin"))
+            {
+                request.Headers.Add("origin", "https://m.bilibili.com");
+                var cookies = LoginHelper.GetLoginInfoCookiesString();
+                if (cookies is not "")
+                {
+                    request.Headers.Add("cookie", cookies);
+                }
+            }
+
+            var response = _httpClient.Send(request);
+            response.EnsureSuccessStatusCode();
+
+            using var fs = File.Create(destFile);
+            response.Content.ReadAsStream().CopyTo(fs);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("DownloadFile()发生异常: {0}", e);
+            LogManager.Error(e);
+            throw;
+        }
     }
 }
