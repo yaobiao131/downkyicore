d9b1558e75140dc4080045016179506ba90134fc
yaobiao131 <28655758+yaobiao131@users.noreply.github.com>
2025-08-10 17:11:09 +0800

    chore(release): prepare for 1.0.21

--- DownKyi/Utils/ImageHelper.cs
+++ DownKyi/Utils/ImageHelper.cs
@@ -1,7 +1,4 @@
 using System;
-using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 
@@ -27,22 +24,5 @@ public static class ImageHelper
     private static Bitmap LoadFromFile(Uri resourceUri)
     {
         return new Bitmap(resourceUri.LocalPath);
-    }
-
-    public static async Task<Bitmap?> LoadFromWeb(Uri url)
-    {
-        using var httpClient = new HttpClient();
-        try
-        {
-            var response = await httpClient.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-            var data = await response.Content.ReadAsByteArrayAsync();
-            return new Bitmap(new MemoryStream(data));
-        }
-        catch (HttpRequestException ex)
-        {
-            Console.WriteLine($"An error occurred while downloading image '{url}' : {ex.Message}");
-            return null;
-        }
     }
 }
