26480614671db35b5772502dec8139a75fcffe31
Nlick47 <2247717951@qq.com>
2025-09-14 00:49:50 +0800

    refactor: 修改异步命令创建参数

--- DownKyi/Commands/CancellableAsyncDelegateCommand.cs
+++ DownKyi/Commands/CancellableAsyncDelegateCommand.cs
@@ -1,23 +1,27 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace DownKyi.Commands;
 
-public class AsyncDelegateCommand<T> : ICommand
+public class CancellableAsyncDelegateCommand<T> : ICommand
 {
-    private readonly Func<T?, Task> _execute;
+    private readonly Func<T, CancellationToken, Task> _execute;
     private readonly Func<T, bool>? _canExecute;
+    private CancellationTokenSource? _cancellationTokenSource;
     private bool _isExecuting;
 
     public event EventHandler? CanExecuteChanged;
 
-    public AsyncDelegateCommand(Func<T?, Task> execute, Func<T, bool>? canExecute = null)
+    public CancellableAsyncDelegateCommand(
+        Func<T, CancellationToken, Task> execute, 
+        Func<T, bool>? canExecute = null)
     {
         _execute = execute ?? throw new ArgumentNullException(nameof(execute));
         _canExecute = canExecute;
     }
-    
+
     public bool CanExecute(object? parameter)
     {
         if (parameter is not T typedParameter)
@@ -37,33 +41,31 @@ public class AsyncDelegateCommand<T> : ICommand
         _isExecuting = true;
         RaiseCanExecuteChanged();
 
+        _cancellationTokenSource = new CancellationTokenSource();
+
         try
         {
-            await _execute(typedParameter);
+            await _execute(typedParameter, _cancellationTokenSource.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            
         }
         finally
         {
             _isExecuting = false;
+            _cancellationTokenSource = null;
             RaiseCanExecuteChanged();
         }
+    }
+
+    public void Cancel()
+    {
+        _cancellationTokenSource?.Cancel();
     }
 
     protected void RaiseCanExecuteChanged()
     {
         CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-    }
-}
-
-public class AsyncDelegateCommand : AsyncDelegateCommand<object>
-{
-    public AsyncDelegateCommand(Func<object?, Task> execute, Func<object, bool>? canExecute = null)
-        : base(execute, canExecute)
-    {
-    }
-    
-    public AsyncDelegateCommand(Func<Task> execute, Func<bool>? canExecute = null)
-        : this(_ => execute(), 
-            canExecute != null ? _ => canExecute() : null)
-    {
     }
 }
